889ae88f2c006874b50de955a414f8d325cafbef
dharmawipraja <dharmawipraja@gmail.com>
2026-06-19 02:31:33 +0800

    chore(migrations): remove trigram indexes from business partners, journal entries, payments, and purchase bills
    This migration drops several trigram indexes to optimize database performance and reduce unnecessary overhead. The following indexes were removed:
    - business_partners_code_trgm
    - business_partners_email_trgm
    - business_partners_name_trgm
    - business_partners_npwp_trgm
    - journal_entries_description_trgm
    - journal_entries_entry_ref_trgm
    - payments_description_trgm
    - payments_ref_trgm
    - purchase_bills_bill_ref_trgm
    - purchase_bills_description_trgm
    - purchase_bills_vendor_invoice_no_trgm
    - sales_invoices_description_trgm
    - sales_invoices_invoice_ref_trgm

---
+++ prisma/migrations/20260618182249_remove_trigram_indexes/migration.sql
@@ -0,0 +1,38 @@
+-- DropIndex
+DROP INDEX "business_partners_code_trgm";
+
+-- DropIndex
+DROP INDEX "business_partners_email_trgm";
+
+-- DropIndex
+DROP INDEX "business_partners_name_trgm";
+
+-- DropIndex
+DROP INDEX "business_partners_npwp_trgm";
+
+-- DropIndex
+DROP INDEX "journal_entries_description_trgm";
+
+-- DropIndex
+DROP INDEX "journal_entries_entry_ref_trgm";
+
+-- DropIndex
+DROP INDEX "payments_description_trgm";
+
+-- DropIndex
+DROP INDEX "payments_ref_trgm";
+
+-- DropIndex
+DROP INDEX "purchase_bills_bill_ref_trgm";
+
+-- DropIndex
+DROP INDEX "purchase_bills_description_trgm";
+
+-- DropIndex
+DROP INDEX "purchase_bills_vendor_invoice_no_trgm";
+
+-- DropIndex
+DROP INDEX "sales_invoices_description_trgm";
+
+-- DropIndex
+DROP INDEX "sales_invoices_invoice_ref_trgm";
